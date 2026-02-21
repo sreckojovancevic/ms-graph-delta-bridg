ff5cdcdc1f82a2e6f8f96d1e2329546e8548078e
sreckojovancevic <srecko.jovancevic@gmail.com>
2026-02-21 14:54:42 +0100

    Add BridgeLogger class for logging functionality

--- src/Utils.php
+++ src/Utils.php
@@ -1,1 +1,13 @@
+<?php
+declare(strict_types=1);
 
+class BridgeLogger {
+    public static function log(string $message, string $level = 'INFO'): void {
+        $logDir = __DIR__ . '/../logs';
+        if (!is_dir($logDir)) { mkdir($logDir, 0777, true); }
+        
+        $file = $logDir . '/bridge_' . date('Y-m-d') . '.log';
+        $entry = sprintf("[%s] [%s]: %s\n", date('H:i:s'), $level, $message);
+        file_put_contents($file, $entry, FILE_APPEND);
+    }
+}
